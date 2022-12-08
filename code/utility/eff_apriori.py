5b9a390fb921dc7d526d327fedd760ce221aaba3
Vlad Marian Cimpeanu <62434812+VladMarianCimpeanu@users.noreply.github.com>
2022-12-08 14:28:31 +0100

    added possibility to change itemset size when using apriori algorithm

--- code/utility/eff_apriori.py
+++ code/utility/eff_apriori.py
@@ -1,6 +1,6 @@
 import efficient_apriori
 
-def rules(store_data, support, confidence):
+def rules(store_data, support, confidence, max_itemset_cardinality=8):
 
     #every tuple is a record, the index of the column is also stored
     records = []
@@ -8,6 +8,6 @@ def rules(store_data, support, confidence):
         records.append([(str(store_data.values[i,j]), j) for j in range(0, len(store_data.columns))])
 
     #find the rules using apriori algorithm
-    itemset, rules = efficient_apriori.apriori(records, min_support=support, min_confidence=confidence)
-    print(rules)
+    itemset, rules = efficient_apriori.apriori(records, min_support=support, min_confidence=confidence, max_length=max_itemset_cardinality)
+    # print(rules)
     return rules
